9a61b0843a9cf2569fcd0e3544479faaaa60059d
lsdongcom <havenzhang@qq.com>
2018-05-22 12:04:55 +0800

    update

--- controller/help.py
+++ controller/help.py
@@ -2,13 +2,20 @@
 
 import sys
 from base import BaseHandler
-from settings import siteinfo
 from utils.file_helper import lock_site_notify
+from settings import siteinfo
+from settings import wechatapi
+from urllib import parse
 sys.path.append('..')
 
 class HelpHandler(BaseHandler):
 
     def get(self,content):
         islock, site_notify = lock_site_notify()
-        self.render(content, site_notify=site_notify, siteinfo=siteinfo)
+        user = self.get_current_user()
+        data = wechatapi
+        if user:
+            self.render(content, data=data, returnurl = parse.quote(wechatapi['redirect_uri']), username=user.nickname, site_notify=site_notify, siteinfo=siteinfo)
+        else:
+            self.render(content, data=data, returnurl = parse.quote(wechatapi['redirect_uri']), username=None, site_notify=site_notify, siteinfo=siteinfo)
 
